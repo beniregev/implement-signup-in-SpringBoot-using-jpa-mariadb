0117764399a4555219ea1c94a2d4cd3778f6a28b
binyamin.regev <beniregev@gmail.com>
2022-04-16 14:23:49 +0300

    Initial commit

---
+++ src/main/java/com/example/implement_signup_in_springboot_using_jpa_mariadb/ImplementSignupInSpringBootUsingJpaMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.example.implement_signup_in_springboot_using_jpa_mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementSignupInSpringBootUsingJpaMariadbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementSignupInSpringBootUsingJpaMariadbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/implement_signup_in_springboot_using_jpa_mariadb/ImplementSignupInSpringBootUsingJpaMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implement_signup_in_springboot_using_jpa_mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementSignupInSpringBootUsingJpaMariadbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
