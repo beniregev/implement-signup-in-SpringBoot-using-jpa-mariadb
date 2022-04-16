9d4eac81fdc76a3041aae538ae4dc98d7e0ef813
binyamin.regev <beniregev@gmail.com>
2022-04-16 14:30:43 +0300

    Adding UserService class implementing IUserService interface to service package

---
+++ src/main/java/com/example/implement_signup_in_springboot_using_jpa_mariadb/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.implement_signup_in_springboot_using_jpa_mariadb.service;
+
+import com.example.implement_signup_in_springboot_using_jpa_mariadb.model.UserDTO;
+import com.example.implement_signup_in_springboot_using_jpa_mariadb.model.UserEntity;
+import com.example.implement_signup_in_springboot_using_jpa_mariadb.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserEntity createUser(UserDTO pDTO) throws Exception {
+        log.info(this.getClass().getName() + ".createUser(UserDTO) Start");
+
+        pDTO.setUserPassword("EncPassword");
+        pDTO.setUserStatus(1);
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        UserEntity pEntity = mapper.map(pDTO, UserEntity.class);
+        return userRepository.save(pEntity);
+    }
+
+}
